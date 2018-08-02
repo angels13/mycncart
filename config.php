ba842112353a8b3aaf9babc152fdc1b7ba871e07
joytaobao <joy@ngmap.com>
2018-08-02 09:01:53 +0800

    根目录

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://trade.ngmap.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://trade.ngmap.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ngmapco2/public_html/trade.ngmap.com/catalog/');
+define('DIR_SYSTEM', '/home/ngmapco2/public_html/trade.ngmap.com/system/');
+define('DIR_LANGUAGE', '/home/ngmapco2/public_html/trade.ngmap.com/catalog/language/');
+define('DIR_TEMPLATE', '/home/ngmapco2/public_html/trade.ngmap.com/catalog/view/theme/');
+define('DIR_CONFIG', '/home/ngmapco2/public_html/trade.ngmap.com/system/config/');
+define('DIR_IMAGE', '/home/ngmapco2/public_html/trade.ngmap.com/image/');
+define('DIR_CACHE', '/home/ngmapco2/public_html/trade.ngmap.com/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/ngmapco2/public_html/trade.ngmap.com/system/storage/download/');
+define('DIR_LOGS', '/home/ngmapco2/public_html/trade.ngmap.com/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/ngmapco2/public_html/trade.ngmap.com/system/storage/modification/');
+define('DIR_UPLOAD', '/home/ngmapco2/public_html/trade.ngmap.com/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ngmapco2_opcart');
+define('DB_PASSWORD', '19860628a');
+define('DB_DATABASE', 'ngmapco2_opcart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'mcc_');
